d5a76d81245c7388370c555b0c8d6aeb574807ed
Carlos Rodas <crods57@gmail.com>
2020-10-01 07:45:59 -0500

    Added usuario service

---
+++ canchas/src/usuario/usuario.service.ts
@@ -0,0 +1,48 @@
+import {Injectable} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {FindManyOptions, Like, Repository} from "typeorm";
+import {UsuarioEntity} from "./usuario.entity";
+
+@Injectable()
+export class UsuarioService {
+    constructor(
+        @InjectRepository(UsuarioEntity)
+        private repositorio: Repository<UsuarioEntity>
+    ){
+    }
+    crearUno(nuevoUsuario: UsuarioEntity){
+        return this.repositorio.save(nuevoUsuario) //Promesa
+    }
+    buscarTodos(textoDeConsulta?: string){
+        const consulta: FindManyOptions<UsuarioEntity> = {
+            where: [
+                {
+                    numeroCedula: Like(`%${textoDeConsulta}`)
+                },
+                {
+                    numeroPasaporte: Like(`%${textoDeConsulta}`)
+                },
+                {
+                    numeroRuc: Like(`%${textoDeConsulta}`)
+                },
+                {
+                    nombre: Like(`%${textoDeConsulta}`)
+                },
+                {
+                    apellido: Like(`%${textoDeConsulta}`)
+                }
+            ]
+        }
+        return this.repositorio.find(consulta) //Promesa
+    }
+    buscarUno(id: number){
+        return this.repositorio.findOne(id) //Promesa
+    }
+    editarUno(usuarioEditado: UsuarioEntity){
+        return this.repositorio.save(usuarioEditado);
+    }
+    eliminarUno(id: number){
+        return this.repositorio.delete(id);
+    }
+
+}
