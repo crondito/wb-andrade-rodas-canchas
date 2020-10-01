f0aa4bee4a133f4c83a32f638b72d7ba780e6f13
Carlos Rodas <crods57@gmail.com>
2020-10-01 16:56:05 -0500

    Login terminado

--- canchas/src/app.controller.ts
+++ canchas/src/app.controller.ts
@@ -1,6 +1,6 @@
-import {Controller, Get, Res} from '@nestjs/common';
+import {Body, Controller, Get, Post, Req, Res, Session} from '@nestjs/common';
 import { AppService } from './app.service';
-import {CanchaService} from "./cancha/cancha.service";
+import { CanchaService } from "./cancha/cancha.service";
 
 @Controller()
 export class AppController {
@@ -14,25 +14,94 @@ export class AppController {
   getHello(
       @Res() res,
   ){
-    res.redirect('home')
+    res.redirect('/login')
   }
 
-  @Get("home")
+  @Get('home')
   async vistaHome(
-      @Res() res
+      @Res() res,
+      @Session() session,
   ){
-    let canchasEncontradas
+    let canchasEncontradas;
+    const estaLogeado = session.usuario;
     try {
       canchasEncontradas = await this._canchaService.buscarCanchasDisponibles()
     }catch (error){
       canchasEncontradas = "0"
     }
-    res.render(
-        "principal/home",
-        {
-          numeroCanchas: canchasEncontradas[1]
-        }
-    )
+    if (estaLogeado) {
+      return res.render(
+          'principal/home',
+          {
+            usuario: session.usuario,
+            roles: session.roles
+          }
+      )
+    } else {
+      return res.redirect('/login')
+    }
+    // res.render(
+    //     "principal/home",
+    //     {
+    //       numeroCanchas: canchasEncontradas[1]
+    //     }
+    // )
+  }
+
+  @Get('login')
+  login(
+      @Res() response
+  ) {
+    return response.render('login/login')
+  }
+
+  @Post('login')
+  loginPost(
+      @Body() parametrosConsulta,
+      @Res() response,
+      @Session() session
+  ) {
+    // validamos datos
+    const usuario = parametrosConsulta.usuario;
+    const password = parametrosConsulta.password;
+    if (usuario == 'admin' && password == 'admin') {
+      session.usuario = usuario
+      session.roles = ['Administrador']
+      return response.redirect('home');
+    } else {
+          return response.redirect('login')
+      }
+    }
+
+  @Get('protegido')
+  protegido(
+      @Res() response,
+      @Session() session,
+  ) {
+    const estaLogeado = session.usuario;
+    if (estaLogeado) {
+      return response.render(
+          'login/protegido',
+          {
+            usuario: session.usuario,
+            roles: session.roles
+          }
+      )
+    } else {
+      return response.redirect('/login')
+    }
+  }
+
+  @Get('logout')
+  logout(
+      @Session() session,
+      @Res() response,
+      @Req() request
+  ) {
+    session.username = undefined;
+    session.roles = undefined;
+    request.session.destroy();
+    return response.redirect('login')
   }
 
 }
--- canchas/src/main.ts
+++ canchas/src/main.ts
@@ -3,11 +3,27 @@ import { AppModule } from './app.module';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require("express");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const session = require('express-session')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const FileStore = require('session-file-store')(session)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule) as any;
+  const secret = 'web'
   app.set("view engine","ejs")
   app.use(express.static("public"))
+  app.use(
+      session({
+        name: 'server-session-id',
+        secret: secret,
+        resave: true,
+        saveUninitialized: true,
+        cookie: {secure: false},
+        store: new FileStore(),
+      }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
